54f1b5fadede73e7af5924df9843398d407b1a81
Ruben Perez <rubenpm88@gmail.com>
2017-10-10 22:46:15 +0200

    pasar de js a cs Botones

---
+++ Assets/Scripts/Botones.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Botones : MonoBehaviour {
+
+
+	public void LoadScene() {
+		Application.LoadLevel("BasicShooting_Test");
+	}
+	
+
+	void BotonSalir () {
+		Application.Quit();
+	}
+}
+
