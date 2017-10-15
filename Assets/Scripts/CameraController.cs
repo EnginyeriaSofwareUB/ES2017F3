a15013b92a8b05861fc116e21cbd50d457413521
Astor Prieto <astorprieto@gmail.com>
2017-10-15 19:37:38 +0200

    -- CameraController added, camera now zooms, and pressing Tab changes follow player/free mouse modes.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	private GameController controller;
+	private FollowingCamera f;
+	private MovementCamera m;
+
+	public bool follow = true;
+
+	void Awake () {
+		// Acceso al Controlador, Camara y guardamos la referencia al objeto Jugador.
+		controller = GameObject.FindGameObjectWithTag("GM").GetComponent<GameController>();
+		f = Camera.main.GetComponent<FollowingCamera> ();
+		m = Camera.main.GetComponent<MovementCamera> ();
+
+		f.target = controller.activePlayer;
+	}
+
+	// Use this for initialization
+	void Start () {
+		if (follow) {
+			m.enabled = false;
+			f.enabled = true;
+		} else {
+			m.enabled = true;
+			f.enabled = false;
+		}
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		// Pressing Tab Key makes lock/unlock character
+		if (Input.GetKeyDown(KeyCode.Tab)) {
+			follow = !follow;
+		}
+
+		if (follow) {
+			m.enabled = false;
+			f.enabled = true;
+		} else {
+			m.enabled = true;
+			f.enabled = false;
+		}
+	}
+}
--- Assets/Scripts/FollowingCamera.cs
+++ Assets/Scripts/FollowingCamera.cs
@@ -4,26 +4,21 @@ using UnityEngine;
 
 public class FollowingCamera : MonoBehaviour {
 
-	private GameController controller;
-	private Camera camera;
-
 	public GameObject target;
 	private Vector3 offset;
 
 	private float dampTime = 0.1f;
 	private Vector3 velocity = Vector3.zero;
+	private Camera camera;
 
 	[Header("Smooth-Follow Camera")]
 	public bool activateSmooth = false;
 
 	// Use this for initialization
 	void Start () {
-		// Acceso al Controlador, Camara y guardamos la referencia al objeto Jugador.
-		controller = GameObject.FindGameObjectWithTag("GM").GetComponent<GameController>();
 		camera = GetComponent<Camera>();
 			
 		// Initial position of the Camera based on activePlayer Position. Initial set of 'offset'.
-        target = controller.activePlayer;
 		if (target) {
 			transform.position = new Vector3 (target.transform.position.x, (target.transform.position.y + 2), transform.position.z);
 			offset = transform.position - target.transform.position;
@@ -36,11 +31,11 @@ public class FollowingCamera : MonoBehaviour {
 
 			// Camera Smooth Damping for a nicer follow.
 			if (activateSmooth) {
-				Vector3 point = camera.WorldToViewportPoint (target.transform.position);
-				Vector3 delta = target.transform.position - camera.ViewportToWorldPoint (new Vector3 (0.45f, 0.25f, point.z));
+				Vector3 point = camera.WorldToViewportPoint(target.transform.position);
+				Vector3 delta = target.transform.position - camera.ViewportToWorldPoint(new Vector3 (0.5f, 0.5f, point.z));
 
-				transform.position = Vector3.SmoothDamp (transform.position, (transform.position + delta), ref velocity, dampTime);
-			
+				transform.position = Vector3.SmoothDamp(transform.position, (transform.position + delta), ref velocity, dampTime);
+
 			// Camera basic follow.
 			} else {
 				transform.position = target.transform.position + offset;
