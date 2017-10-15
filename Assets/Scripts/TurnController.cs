cf40f1d87ce3766f1bcb74267a8b317dbdb81dd5
arkeros <rafael@arque.ro>
2017-10-15 18:16:40 +0200

    turns changing every 10 seconds

---
+++ Assets/Scripts/TurnController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnController : MonoBehaviour {
+
+	private GameController gameController;
+
+	
+
+
+
+	// Use this for initialization
+	void Start () {
+		// chapuza
+		gameController = GameObject.FindGameObjectWithTag("GM").GetComponent<GameController>();
+
+        // foreach (GameObject player in players) {
+        //     // Instantiate(respawnPrefab, respawn.transform.position, respawn.transform.rotation);
+        // }
+		// gameController.players;
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
