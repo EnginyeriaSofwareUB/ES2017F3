87ea1b285d2912d61513a4468bdf4619cf04522d
Astor Prieto <astorprieto@gmail.com>
2017-10-16 23:57:19 +0200

    -- Fix for the following camera script.

--- Assets/Scripts/FollowingCamera.cs
+++ Assets/Scripts/FollowingCamera.cs
@@ -5,42 +5,37 @@ using UnityEngine;
 public class FollowingCamera : MonoBehaviour {
 
 	public GameObject target;
-	private Vector3 offset;
 
-	private float dampTime = 0.1f;
-	private Vector3 velocity = Vector3.zero;
-	private Camera camera;
+	[Header("Camera sets")]
+	public float offset = 20.0f;
+	public float height = 2.0f;
+	public float speed = 3.0f;
 
 	[Header("Smooth-Follow Camera")]
 	public bool activateSmooth = false;
 
 	// Use this for initialization
 	void Start () {
-		camera = GetComponent<Camera>();
-			
-
+		transform.position = new Vector3 (target.transform.position.x, (target.transform.position.y + 2), transform.position.z);
 	}
 	
 	// LateUpdate is called once per frame
 	void LateUpdate () {
-		// Initial position of the Camera based on activePlayer Position. Initial set of 'offset'.
-		if (target) {
-			transform.position = new Vector3 (target.transform.position.x, (target.transform.position.y + 2), transform.position.z);
-			offset = transform.position - target.transform.position;
-		}
 
 		if (target) {
-
 			// Camera Smooth Damping for a nicer follow.
 			if (activateSmooth) {
-				Vector3 point = camera.WorldToViewportPoint(target.transform.position);
-				Vector3 delta = target.transform.position - camera.ViewportToWorldPoint(new Vector3 (0.5f, 0.5f, point.z));
 
-				transform.position = Vector3.SmoothDamp(transform.position, (transform.position + delta), ref velocity, dampTime);
+				Vector3 pos = transform.position;
+				pos.x = Mathf.Lerp(transform.position.x, target.transform.position.x, (speed * Time.deltaTime));
+				pos.y = Mathf.Lerp(transform.position.y, (target.transform.position.y + height), (speed * Time.deltaTime));
+				pos.z = target.transform.position.z - offset;
+
+				transform.position = pos;
 
 			// Camera basic follow.
 			} else {
-				transform.position = target.transform.position + offset;
+				transform.position = new Vector3(target.transform.position.x, target.transform.position.y + height, target.transform.position.z - offset);
 			}
 		}
 	}
