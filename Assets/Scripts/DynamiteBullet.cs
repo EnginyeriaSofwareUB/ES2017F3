e36fef53dc06391e7bbd600b6493c75d74972e96
Albert <agarcisa81@alumnes.ub.edu>
2017-10-23 16:16:30 +0200

    Dynamite explosion trigger event fixed.

--- Assets/Scripts/DynamiteBullet.cs
+++ Assets/Scripts/DynamiteBullet.cs
@@ -13,22 +13,7 @@ public class DynamiteBullet : ExplosiveBullet {
 	}
 
     protected override void DespawnBullet() {
-        StartCoroutine("DynamiteExplosion");
-    }
-
-    IEnumerator DynamiteExplosion() {
         anim.SetTrigger("explode");
-
-        //AnimatorStateInfo animState = anim.GetCurrentAnimatorStateInfo(0);
-        //while (animState.normalizedTime < 1) {
-        //    yield return null;
-        //}
-
-        //Debug.Log(anim.GetCurrentAnimatorStateInfo(0).length);
-        yield return new WaitForSeconds(1.4f);
-        
-        TriggerExplosion();
-
     }
 
 }
