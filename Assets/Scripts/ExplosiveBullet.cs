fd914711ce41f3422b88c8a5b054dc0089a70ca6
jordiolivares <jordi@jolivar.es>
2017-11-14 23:51:01 +0100

    Fix build in Linux systems

--- Assets/Scripts/ExplosiveBullet.cs
+++ Assets/Scripts/ExplosiveBullet.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.InteropServices;
-using NUnit.Framework.Constraints;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class ExplosiveBullet : AbstractBullet
 {
