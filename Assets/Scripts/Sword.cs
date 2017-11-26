b75300e4628ea02faa3a971ac968334d52e231f4
jordiolivares <jordi@jolivar.es>
2017-11-26 13:17:46 +0100

    Created the light sword and made some prefabs to be at 0

--- Assets/Scripts/Sword.cs
+++ Assets/Scripts/Sword.cs
@@ -3,31 +3,9 @@ using UnityEngine;
 
 public class Sword : Gun
 {
-    private Collider _hurtingObjectCollider;
-    
-    private void Start()
-    {
-        base.Start();
-        foreach (Transform child in transform)
-        {
-            if (child.CompareTag("Hurting Object"))
-                _hurtingObjectCollider = child.GetComponent<MeshCollider>();
-        }
-        DisableDamage();
-    }
 
     public override void Shoot(float thrust, float maxPower)
     {
         anim.SetTrigger("attack");
-    }
-
-    public void ActivateDamage()
-    {
-        _hurtingObjectCollider.enabled = true;
-    }
-
-    public void DisableDamage()
-    {
-        _hurtingObjectCollider.enabled = false;
     }
 }
