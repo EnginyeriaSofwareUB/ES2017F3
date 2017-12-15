5b96e860c05b164b13da8c1717aa1f87f7c32e23
jordiolivares <jordi@jolivar.es>
2017-12-15 21:03:04 +0100

    Forgot a most important file, my dudes

---
+++ Assets/Scripts/MoveToFitScreen.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveToFitScreen : MonoBehaviour
+{	
+	// Use this for initialization
+	void Start ()
+	{
+		var minimapHeight = Camera.main.GetComponent<CameraController>().MinimapWidth / Camera.main.aspect;
+		Debug.Log("Calculated height for minimap = " + minimapHeight);
+		var newPosition = transform.position;
+		var distanceToSea = Mathf.Abs(GameObject.FindWithTag("Sea").transform.position.y - newPosition.y);
+		Debug.Log("Distance from Minimap to Sea in height = " + distanceToSea);
+		newPosition.y += minimapHeight - distanceToSea + 0.5f; // Plus a bit to avoid the animation showing emptiness
+		transform.position = newPosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
