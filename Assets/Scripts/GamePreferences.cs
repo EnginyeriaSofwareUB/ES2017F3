190c1f546a060aa33771ddcd204d8b71951b3976
Pere Gilabert <perecasxiru@hotmail.com>
2017-12-18 13:14:29 +0100

    All fixed and changed: #175

--- Assets/Scripts/GamePreferences.cs
+++ Assets/Scripts/GamePreferences.cs
@@ -7,8 +7,8 @@ public class GamePreferences {
 	public static string p1_faction = "pirates";
 	public static string p2_faction = "vikings";
 	public static bool sudden_death_activated = true;
-	public static int sudden_death_turns = 20;
-	public static int number_players_team = 2;
+	public static int sudden_death_turns = 12;
+	public static int number_players_team = 3;
 	public static int players_maxlife = 100;
 	public static int difficulty = 1;
 	public static bool howTo = false;
